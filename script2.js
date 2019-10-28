882c35eb432fe5ba3ce8cb254dc8838d8d6c23c8
rapdan <rapdan@gmail.com>
2019-10-28 07:43:28 +0100

    added directionByKeyCode

--- script2.js
+++ script2.js
@@ -36,190 +36,227 @@
 // funkcja getPhraseByNum(n) tworzy fraze z podanego numeru (od 0 do 9999) np. getPhraseByNum(2567)
 // zwróci dwa tysiące pięćset sześćdziesiąt siedem
 // wykorzystamy funkcję z Cwiczenia 4:
-const genitive = (n, word) => {
-    let res = word.many2; //liczba mnoga słowa
-    if (n % 100 < 5 || n % 100 > 2) {
-        if (n == 1) res = word.singular; // liczba pojedyńcz
-        if (n % 10 > 1 && n % 10 < 5) res = word.many1; // liczba mnoga do 4
-    }
-    return `${res}`;
-}
-const getPhraseByNum = (n) => {
-    let res = "";
-    if (n > 999) { // określamy tysiące
-        let t = +n.toString()[0];
-        switch (t) {
-            case 1:
-                res += "jeden ";
-                break;
-            case 2:
-                res += "dwa ";
-                break;
-            case 3:
-                res += "trzy ";
-                break;
-            case 4:
-                res += "cztery ";
-                break;
-            case 5:
-                res += "pięć ";
-                break;
-            case 6:
-                res += "sześć ";
-                break;
-            case 7:
-                res += "siedem ";
-                break;
-            case 8:
-                res += "osiem ";
-                break;
-            case 9:
-                res += "dziewieć ";
-                break;
-        }
-        res += genitive(t, {
-            singular: "tysiąc",
-            many1: "tysiące",
-            many2: "tysięcy"
-        }) + " ";
-    }
+// const genitive = (n, word) => {
+//     let res = word.many2; //liczba mnoga słowa
+//     if (n % 100 < 5 || n % 100 > 2) {
+//         if (n == 1) res = word.singular; // liczba pojedyńcz
+//         if (n % 10 > 1 && n % 10 < 5) res = word.many1; // liczba mnoga do 4
+//     }
+//     return `${res}`;
+// }
+// const getPhraseByNum = (n) => {
+//     let res = "";
+//     if (n > 999) { // określamy tysiące
+//         let t = +n.toString()[0];
+//         switch (t) {
+//             case 1:
+//                 res += "jeden ";
+//                 break;
+//             case 2:
+//                 res += "dwa ";
+//                 break;
+//             case 3:
+//                 res += "trzy ";
+//                 break;
+//             case 4:
+//                 res += "cztery ";
+//                 break;
+//             case 5:
+//                 res += "pięć ";
+//                 break;
+//             case 6:
+//                 res += "sześć ";
+//                 break;
+//             case 7:
+//                 res += "siedem ";
+//                 break;
+//             case 8:
+//                 res += "osiem ";
+//                 break;
+//             case 9:
+//                 res += "dziewieć ";
+//                 break;
+//         }
+//         res += genitive(t, {
+//             singular: "tysiąc",
+//             many1: "tysiące",
+//             many2: "tysięcy"
+//         }) + " ";
+//     }
 
-    if (n % 1000 > 99) { // określamy setki
-        let t = +n.toString().slice(-3, -2); // ilośc setek
-        switch (t) {
-            case 1:
-                res += "sto ";
-                break;
-            case 2:
-                res += "dwieście ";
-                break;
-            case 3:
-                res += "trzysta ";
-                break;
-            case 4:
-                res += "czterysta ";
-                break;
-            case 5:
-                res += "pięćset ";
-                break;
-            case 6:
-                res += "sześćset ";
-                break;
-            case 7:
-                res += "siedmset ";
-                break;
-            case 8:
-                res += "osiemset ";
-                break;
-            case 9:
-                res += "dziewięćset ";
-                break;
-        }
-    }
-    if (n % 100 > 19) { // określamy dziesiątki
-        let t = +n.toString().slice(-2, -1); //ilość setek
-        switch (t) {
-            case 1:
-                res += "dziesięć ";
-                break;
-            case 2:
-                res += "dwadzieścia ";
-                break;
-            case 3:
-                res += "trzydzieści ";
-                break;
-            case 4:
-                res += "czterdzieści ";
-                break;
-            case 5:
-                res += "pięćdziesiąt ";
-                break;
-            case 6:
-                res += "sześćdziesiąt ";
-                break;
-            case 7:
-                res += "siedemdziesiąt ";
-                break;
-            case 8:
-                res += "osiemdziesiąt ";
-                break;
-            case 9:
-                res += "dziewięćdziesiąt ";
-                break;
-        }
-    }
-    if (n % 100 > 9 && n % 100 < 20) {
-        let t = +n.toString().slice(-2);
-        switch (t) {
-            case 10:
-                res += "dziesięć ";
-                break;
-            case 11:
-                res += "jedenaście ";
-                break;
-            case 12:
-                res += "dwanaście ";
-                break;
-            case 13:
-                res += "trzynaście ";
-                break;
-            case 14:
-                res += "czternaście ";
-                break;
-            case 15:
-                res += "piętnaście ";
-                break;
-            case 16:
-                res += "szesnaście ";
-                break;
-            case 17:
-                res += "siedmnaście ";
-                break;
-            case 18:
-                res += "osiemnaście ";
-                break;
-            case 19:
-                res += "dziewiętnaście ";
-                break;
-        }
-    } else {
-        switch (n % 10) {
-            case 1:
-                res += "jeden ";
-                break;
-            case 2:
-                res += "dwa ";
-                break;
-            case 3:
-                res += "trzy ";
-                break;
-            case 4:
-                res += "cztery ";
-                break;
-            case 5:
-                res += "pięć ";
-                break;
-            case 6:
-                res += "sześć ";
-                break;
-            case 7:
-                res += "siedem ";
-                break;
-            case 8:
-                res += "osiem ";
-                break;
-            case 9:
-                res += "dziewięć ";
-                break;
-        }
-    }
-    if (!n) res = "zero ";
-    return res.slice(0, -1); // usuwamy ostatnią spację
-}
+//     if (n % 1000 > 99) { // określamy setki
+//         let t = +n.toString().slice(-3, -2); // ilośc setek
+//         switch (t) {
+//             case 1:
+//                 res += "sto ";
+//                 break;
+//             case 2:
+//                 res += "dwieście ";
+//                 break;
+//             case 3:
+//                 res += "trzysta ";
+//                 break;
+//             case 4:
+//                 res += "czterysta ";
+//                 break;
+//             case 5:
+//                 res += "pięćset ";
+//                 break;
+//             case 6:
+//                 res += "sześćset ";
+//                 break;
+//             case 7:
+//                 res += "siedmset ";
+//                 break;
+//             case 8:
+//                 res += "osiemset ";
+//                 break;
+//             case 9:
+//                 res += "dziewięćset ";
+//                 break;
+//         }
+//     }
+//     if (n % 100 > 19) { // określamy dziesiątki
+//         let t = +n.toString().slice(-2, -1); //ilość setek
+//         switch (t) {
+//             case 1:
+//                 res += "dziesięć ";
+//                 break;
+//             case 2:
+//                 res += "dwadzieścia ";
+//                 break;
+//             case 3:
+//                 res += "trzydzieści ";
+//                 break;
+//             case 4:
+//                 res += "czterdzieści ";
+//                 break;
+//             case 5:
+//                 res += "pięćdziesiąt ";
+//                 break;
+//             case 6:
+//                 res += "sześćdziesiąt ";
+//                 break;
+//             case 7:
+//                 res += "siedemdziesiąt ";
+//                 break;
+//             case 8:
+//                 res += "osiemdziesiąt ";
+//                 break;
+//             case 9:
+//                 res += "dziewięćdziesiąt ";
+//                 break;
+//         }
+//     }
+//     if (n % 100 > 9 && n % 100 < 20) {
+//         let t = +n.toString().slice(-2);
+//         switch (t) {
+//             case 10:
+//                 res += "dziesięć ";
+//                 break;
+//             case 11:
+//                 res += "jedenaście ";
+//                 break;
+//             case 12:
+//                 res += "dwanaście ";
+//                 break;
+//             case 13:
+//                 res += "trzynaście ";
+//                 break;
+//             case 14:
+//                 res += "czternaście ";
+//                 break;
+//             case 15:
+//                 res += "piętnaście ";
+//                 break;
+//             case 16:
+//                 res += "szesnaście ";
+//                 break;
+//             case 17:
+//                 res += "siedmnaście ";
+//                 break;
+//             case 18:
+//                 res += "osiemnaście ";
+//                 break;
+//             case 19:
+//                 res += "dziewiętnaście ";
+//                 break;
+//         }
+//     } else {
+//         switch (n % 10) {
+//             case 1:
+//                 res += "jeden ";
+//                 break;
+//             case 2:
+//                 res += "dwa ";
+//                 break;
+//             case 3:
+//                 res += "trzy ";
+//                 break;
+//             case 4:
+//                 res += "cztery ";
+//                 break;
+//             case 5:
+//                 res += "pięć ";
+//                 break;
+//             case 6:
+//                 res += "sześć ";
+//                 break;
+//             case 7:
+//                 res += "siedem ";
+//                 break;
+//             case 8:
+//                 res += "osiem ";
+//                 break;
+//             case 9:
+//                 res += "dziewięć ";
+//                 break;
+//         }
+//     }
+//     if (!n) res = "zero ";
+//     return res.slice(0, -1); // usuwamy ostatnią spację
+// }
 
 // Test:
-console.log(getPhraseByNum(4517));
-console.log(getPhraseByNum(9900));
-console.log(getPhraseByNum(78));
-console.log(getPhraseByNum(0));
-console.log(getPhraseByNum(4564));
+// console.log(getPhraseByNum(4517));
+// console.log(getPhraseByNum(9900));
+// console.log(getPhraseByNum(78));
+// console.log(getPhraseByNum(0));
+// console.log(getPhraseByNum(4564));
+
+
+
+
+// Cwiczenie 9
+// funkcja directionByKeyCode(keyCode) przyjmuje jedną z wartosci: 87, 65, 68, 83.
+// ma zwracać odpowiednio: "góra ↑", "lewo ←", "prawo →", "dół ↓"
+// np. directionByKeyCode(87) powinien zwrócić: "góra ↑"
+// const directionByKeyCode = (keyCode) => {
+//     switch (keyCode) {
+//         case 87:
+//             return "góra ↑";
+//             break;
+//         case 65:
+//             return "lewo ←";
+//             break;
+//         case 68:
+//             return "prawo →";
+//             break;
+//         case 83:
+//             return "dół ↓";
+//             break;
+//     }
+// }
+// lub =============================================
+const directionByKeyCode = (keyCode) => ({
+    '87': 'góra ↑',
+    '65': 'lewo ←',
+    '68': 'prawo →',
+    '83': 'dół ↓',
+})[keyCode];
+// Test:
+console.log(directionByKeyCode(87));
+console.log(directionByKeyCode(68));
+console.log(directionByKeyCode(83));
+console.log(directionByKeyCode(65));
+console.log(directionByKeyCode(34));
